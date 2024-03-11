845e25a0de3c9376070ace9552c7286fd9a60e4f
teotia-codes <priyanshu.teotia50@gmail.com>
2024-03-12 00:17:18 +0530

    done with adding utils like flushbar , toast

---
+++ lib/utils/utils.dart
@@ -0,0 +1,30 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+
+
+class Utils {
+
+
+
+  static toastMessage(String message){
+    
+  }
+  static  flushBarErrorMessage(String message, BuildContext context){
+   showFlushbar(context: context, 
+   flushbar: Flushbar(
+    message: message,
+    borderRadius: BorderRadius.circular(8),
+    forwardAnimationCurve: Curves.decelerate,
+    margin: const EdgeInsets.symmetric(horizontal: 20,
+    vertical: 10),
+    padding: const EdgeInsets.all(12),
+    duration: const Duration(seconds: 3),
+    backgroundColor: Colors.red,
+    reverseAnimationCurve: Curves.easeOut,
+    positionOffset: 20,
+   )..show(context),
+   ) ;
+}
+}
+
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm/utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,8 +13,14 @@ class  LoginScreenState extends State <LoginScreen> {
   Widget build(BuildContext context) {
 return Scaffold(
   body: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
 children: [
-  
+  InkWell(
+    onTap: () {
+      Utils.flushBarErrorMessage('Error occured', context);
+    },
+    child:const Center(child: Text("Click ME")),
+  )
 ],
   ),
 );
